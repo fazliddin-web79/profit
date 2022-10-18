32063c177c1b87b322022b534a151a9ce54d7e82
fazliddin-web79 <yakubjonovfazliddin777@gmail.com>
2022-10-18 07:03:19 -0700

    first component

---
+++ src/components/homeBody/index.js
@@ -0,0 +1,341 @@
+import React from "react";
+import Weater from "../weater";
+
+const HomeBody = () => {
+  return (
+    <main>
+      <div className="container">
+        <div className="sponsored-wrapper">
+          <div className="sponsored-box sponsored-box-1">
+            <div className="sponsor-left">
+              <div className="sponsor-category sponsor-category-1">
+                TECHNOLOGY
+              </div>
+              <a className="sponsor-title" href="/">
+                Apple Watch Series 9 Reportedly Has Flat Sides and Bigger
+                Screens
+              </a>
+              <div className="sponsor-user">
+                <div className="sponsor-user-img">
+                  <img
+                    src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/av2-60x60.jpg"
+                    alt="userImg"
+                  />
+                </div>
+                <div className="sponsor-user-title">Jacob</div>
+                <p>Sep 19, 2021</p>
+                <div className="sponsor-save-item">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 24 24"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g>
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="sponsor-right">
+              <img
+                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/t12-330x220.jpg"
+                alt="sponsor1"
+              />
+            </div>
+          </div>
+          <div className="sponsored-box sponsored-box-2">
+            <div className="sponsor-left">
+              <div className="sponsor-category sponsor-category-2">TRAVEL</div>
+              <a className="sponsor-title" href="/">
+                REVIEW: San Diego City Beat’s a Night at The Besties
+              </a>
+              <div className="sponsor-bottom">
+                <div className="sponsor-colors">
+                  <span className="sponsor-color1"></span>
+                  <span className="sponsor-color2"></span>
+                  <span className="sponsor-color3"></span>
+                  <span className="sponsor-color4"></span>
+                  <span className="sponsor-color5"></span>
+                </div>
+                <b>9.6</b>
+                <span>out of 10</span>
+                <div className="sponsor-recomend">Good Place</div>
+                <div className="sponsor-save-item">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 24 24"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g>
+                      <path fill="none" d="M0 0h24v24H0z"></path>
+                      <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="sponsor-right">
+              <img
+                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/v4-330x220.jpg"
+                alt="sponsor2"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sponsor-ad-title">- Sponsored -</div>
+        <div className="sponsor-ad-main">
+          <img
+            src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/728ad.jpg"
+            alt="ad"
+          />
+        </div>
+      </div>
+      <div className="container">
+        <div className="editor-pick-header">
+          <span>/ </span>Editor's Pick<span> /</span>
+        </div>
+        <div className="editor-pick-main">
+          <div className="editor-pick-left">
+            <div className="editor-pick-hero">
+              <div className="editor-pick-hero-left">
+                <a className="editor-pick-hero-title" href="/">
+                  Corsair HS80 RGB Wireless Gaming Headset Review
+                </a>
+                <div className="editor-pick-hero-rating">
+                  <span>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 16 16"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+                    </svg>
+                  </span>
+                  <span>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 16 16"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+                    </svg>
+                  </span>
+                  <span>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 16 16"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+                    </svg>
+                  </span>
+                  <span>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 16 16"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+                    </svg>
+                  </span>
+                  <span>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 16 16"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path>
+                    </svg>
+                  </span>
+                  <b>4.8</b>
+                  <span className="editor-pick-hero-middle">out of 5</span>
+                  <b>Good</b>
+                </div>
+                <p>
+                  As for quality, the HS80's provided clear-cut sound with
+                  adequate bass and…
+                </p>
+                <div className="editor-pick-hero-bottom">
+                  <div className="editor-pick-hero-user-img">
+                    <img
+                      src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/emm-60x60.jpg"
+                      alt="user"
+                    />
+                  </div>
+                  <b>
+                    <a href="/">Taylor Emma</a>
+                  </b>
+                  <span>Aug 18, 2021</span>
+                  <div className="editor-pick-mark">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 24 24"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g>
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="editor-pick-hero-right">
+                <div className="editor-pick-category">TECHNOLOGY</div>
+              </div>
+            </div>
+            <div className="editor-pick-items">
+              <div className="editor-pick-item">
+                <a className="editor-pick-item-img" href="/">
+                  <img
+                    src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/t13-330x220.jpg"
+                    alt="Self-Driving Cars: Everything You Need to Know"
+                  />
+                </a>
+                <a
+                  className="editor-pick-item-category editor-pick-item-category-1"
+                  href="/"
+                >
+                  TECHNOLOGY
+                </a>
+                <a className="editor-pick-item-title" href="/">
+                  Self-Driving Cars: Everything You Need to Know
+                </a>
+                <div className="editor-pick-item-bottom">
+                  <span>Aug 7, 2021</span>
+                  <span className="editor-pick-item-mark">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 24 24"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g>
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+                      </g>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+              <div className="editor-pick-item">
+                <a className="editor-pick-item-img" href="/">
+                  <img
+                    src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/c14-330x220.jpg"
+                    alt="Girls in Ocean Science Conference a First at Maritime Museum"
+                  />
+                </a>
+                <a
+                  className="editor-pick-item-category editor-pick-item-category-2"
+                  href="/"
+                >
+                  SCIENCE
+                </a>
+                <a className="editor-pick-item-title" href="/">
+                  Girls in Ocean Science Conference a First at Maritime Museum
+                </a>
+                <div className="editor-pick-item-bottom">
+                  <span>Aug 6, 2021</span>
+                  <span className="editor-pick-item-mark">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 24 24"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g>
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+                      </g>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+              <div className="editor-pick-item">
+                <a className="editor-pick-item-img" href="/">
+                  <img
+                    src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/08/t5-330x220.jpg"
+                    alt="Google Must Allow Developers to Use Other Payment Systems"
+                  />
+                </a>
+                <a
+                  className="editor-pick-item-category editor-pick-item-category-3"
+                  href="/"
+                >
+                  TECHNOLOGY
+                </a>
+                <a className="editor-pick-item-title" href="/">
+                  Google Must Allow Developers to Use Other Payment Systems
+                </a>
+                <div className="editor-pick-item-bottom">
+                  <span>Aug 6, 2021</span>
+                  <span className="editor-pick-item-mark">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 24 24"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g>
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+                      </g>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="editor-pick-right">
+            <Weater />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default HomeBody;
---
+++ src/components/homeMainNav/index.js
@@ -0,0 +1,171 @@
+import React from "react";
+
+const HomeMenuNav = () => {
+  return (
+    <div className="hero-main">
+      <div className="hero-box hero-box-1">
+        <div className="hero-bg-gradient"></div>
+        <a className="hero-item-category" href="/">
+          TECHNOLOGY
+        </a>
+        <a className="hero-item-title" href="/">
+          How My Phone’s Most Annoying Feature Saved My Life
+        </a>
+        <p>
+          Modern technology has become a total phenomenon for civilization, the
+          defining force of a new social order in which efficiency is no longer
+          an option but a necessity imposed on all human activity.
+        </p>
+        <div className="hero-item-bottom">
+          <div className="hero-bottom-left">
+            <div className="hero-item-author-img">
+              <img
+                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/emm-60x60.jpg"
+                alt="Taylor Emma"
+              />
+            </div>
+            <div className="hero-item-author">
+              <b>Taylor Emma</b>
+            </div>
+            <p>Oct 1, 2021</p>
+          </div>
+          <div title="Save it" className="hero-bottom-right">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="hero-box hero-box-2">
+        <div className="hero-bg-gradient"></div>
+        <a className="hero-item-category" href="/">
+          BUSINESS
+        </a>
+        <a className="hero-item-title" href="/">
+          The impact of COVID-19 on The Airport Business
+        </a>
+        <div className="hero-item-bottom">
+          <div className="hero-bottom-left">
+            <div className="hero-item-author-img">
+              <img
+                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/emm-60x60.jpg"
+                alt="Taylor Emma"
+              />
+            </div>
+            <div className="hero-item-author">
+              <b>Taylor Emma</b>
+            </div>
+            <p>Oct 1, 2021</p>
+          </div>
+          <div title="Save it" className="hero-bottom-right">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="hero-box hero-box-3">
+        <div className="hero-bg-gradient"></div>
+        <a className="hero-item-category" href="/">
+          POLITICS
+        </a>
+        <a className="hero-item-title" href="/">
+          Bad Credit Shouldn’t Affect Health Insurance, Experts Say
+        </a>
+        <div className="hero-item-bottom">
+          <div className="hero-bottom-left">
+            <div className="hero-item-author-img">
+              <img
+                src="https://foxiz.themeruby.com/default/wp-content/uploads/sites/2/2021/09/emm-60x60.jpg"
+                alt="Taylor Emma"
+              />
+            </div>
+            <div className="hero-item-author">
+              <b>Taylor Emma</b>
+            </div>
+            <p>Oct 1, 2021</p>
+          </div>
+          <div title="Save it" className="hero-bottom-right">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="hero-box hero-box-4">
+        <div className="hero-bg-gradient"></div>
+        <a className="hero-item-category" href="/">
+          ENTERTAINMENT
+        </a>
+        <a className="hero-item-title" href="/">
+          Kuo’s Favorite Levi’s Denim Shorts Are 40% Off on Amazon
+        </a>
+        <div className="hero-item-bottom">
+          <div className="hero-bottom-left">
+            <div className="hero-item-author-img">
+              <img
+                src="/static/media/author.727ed21aa60353f1b07c.webp"
+                alt="Taylor Emma"
+              />
+            </div>
+            <div className="hero-item-author">
+              <b>Taylor Emma</b>
+            </div>
+            <p>Oct 1, 2021</p>
+          </div>
+          <div title="Save it" className="hero-bottom-right">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z"></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeMenuNav;
